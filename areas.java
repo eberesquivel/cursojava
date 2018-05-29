9c5d46f2d7fbad3bfcb9890eb416b2519e663baa
Eber Esquivel <eberesquivel@gmail.com>
2018-05-29 15:00:16 -0500

    Uso de bucle while

--- areas.java
+++ areas.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class areas {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Elige una opción: \n1: Cuadrado \n2: Rectangulo \n3:circulo \n4:ciruclo ");
+        System.out.println("Elige una opción: \n1: Cuadrado \n2: Rectangulo \n3:triángulo \n4:círculo ");
         int figura = entrada.nextInt();
         switch(figura){
             case 1:
@@ -19,7 +19,7 @@ public class areas {
             case 3:
             base= Integer.parseInt(JOptionPane.showInputDialog("Introduce la base :"));
             altura= Integer.parseInt(JOptionPane.showInputDialog("Introduce la altura :"));
-            System.out.println("El área del triángulo es: " +(base*altura)/2);
+            System.out.println("El área del triangulo es: " +(base*altura)/2);
             break;      
             case 4:
             int radio= Integer.parseInt(JOptionPane.showInputDialog("El área del circulo es"));
