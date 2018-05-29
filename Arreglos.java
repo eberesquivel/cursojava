d9165fff7d0ce63f9406877996ac238283b76b85
Eber Esquivel <eberesquivel@gmail.com>
2018-05-29 18:59:24 -0500

    Arreglos inicializados con un for

---
+++ Arreglos.java
@@ -0,0 +1,16 @@
+public class Arreglos {
+    public static void main(String[] args) {
+        /*int matriz [] = new int [5];
+        matriz[0]= 3; 
+        matriz[1]= 23; 
+        matriz[2]= 34; 
+        matriz[3]= 23; 
+        matriz[4]= 43; 
+*/
+
+        int matriz []= {1,2,4,5,5,6,6};
+        for(int i=0;i<matriz.length;i++){
+            System.out.println("Valor del indice "+ i+ matriz[i]);
+        }
+    }
+}
