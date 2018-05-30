c5b8c4feb3318a2f4f9acc2b3756253358c8ca60
Eber Esquivel <eberesquivel@gmail.com>
2018-05-30 11:40:12 -0500

    Encapsulación de variables, generación de metodos getters y setters para ruedas y largo, instancia de clase Coche en clase usoCoche

--- poo/Coche.java
+++ poo/Coche.java
@@ -1,12 +1,32 @@
 
 //definición de la clase
 public class Coche {
-    int ruedas;
-    int motor;
-    int largo;
-    int ancho;
-    int peso;
+   private int ruedas;
+   private int motor;
+   private int largo;
+   private int ancho;
+   private int peso;
+//Métodos getters y Setters ruedas
+   public String getRuedas()
+   {
+   	return "La cantidad de ruedas que tiene el coche es:" + this.ruedas;
+   }
 
+   public void setRuedas(int ruedas)
+   {
+   	this.ruedas = ruedas;
+   }
+
+// getters y setter largo
+      public String getLargo()
+	{
+		return "El largo de el coche es: "+this.largo;
+	}
+
+	public void setLargo(int largo)
+	{
+		this.largo = largo;
+	}
 
 //Método Constructor se llama igual que la clase
 
@@ -18,4 +38,7 @@ public Coche() {
       peso =500;
 }
 
+
+
 }
+
--- poo/usoCoche.java
+++ poo/usoCoche.java
@@ -1,8 +1,10 @@
 
 
-public class usoCoche{
+public class usoCoche extends Coche{
     public static void main(String[] args){
         Coche tiida = new Coche(); // Instancia de clase o ejemplar de clase coche
-        System.out.println("Este coche tiene: " +tiida.ruedas+ "ruedas");
+        System.out.println(tiida.getLargo());
+        System.out.println(tiida.getRuedas());
+        // System.out.println("Este coche tiene: " +tiida.ruedas+ "ruedas");
     }
 }
