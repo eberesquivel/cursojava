bbc4acdf6f015b6bbe996efcd8d9111a33cd6f44
Eber Esquivel <eberesquivel@gmail.com>
2018-05-30 10:36:50 -0500

    poo

---
+++ poo/Coche.java
@@ -0,0 +1,21 @@
+
+//definición de la clase
+public class Coche {
+    int ruedas;
+    int motor;
+    int largo;
+    int ancho;
+    int peso;
+
+
+//Método Constructor se llama igual que la clase
+
+public Coche() {
+      ruedas=4;
+      largo=2000;
+      ancho=300;
+      motor=1600;
+      peso =500;
+}
+
+}
---
+++ poo/usoCoche.java
@@ -0,0 +1,8 @@
+
+
+public class usoCoche{
+    public static void main(String[] args){
+        Coche tiida = new Coche(); // Instancia de clase o ejemplar de clase coche
+        System.out.println("Este coche tiene: " +tiida.ruedas+ "ruedas");
+    }
+}
