856032c93c8c1a2333548d7d998efaa1fc5f5fcf
Eber Esquivel <eberesquivel@gmail.com>
2018-06-27 23:45:08 -0500

    manejos deventos con clases adaptadoras en vez de interfaces

--- graficos/Eventos_Ventana.java
+++ graficos/Eventos_Ventana.java
@@ -24,36 +24,53 @@ class MarcoVentana extends JFrame{
         // setTitle("Respondiendo");
         // setBounds(300, 300, 500, 350);
         setVisible(true);
-        //Instancia Clase oyente
-        M_Ventana oyente_ventana = new M_Ventana();
-        addWindowListener(oyente_ventana);
+        //Instancia Clase oyente metodo largo
+        // M_Ventana oyente_ventana = new M_Ventana();
+        // addWindowListener(oyente_ventana);
+        //Intancia clase oyente metodo corto
+        addWindowListener(new M_Ventana());
+
+
     }
 
 
 }
 
-class M_Ventana implements WindowListener{
+// Ejemplo de clase oyente que implementa Interfaz WindowListener y todos sus métodos
+// class M_Ventana implements WindowListener{
 	
-    public void windowActivated(WindowEvent e){
-        System.out.println("Ventana Activada");
-    }
-    public void windowClosed(WindowEvent e){
-        System.out.println("Ventana Cerrada");
-    }
-    public void windowClosing(WindowEvent e){
-        System.out.println("Ventana Cerrandoce");
-    }
-    public void windowDeactivated(WindowEvent e){
-        System.out.println("Ventana Desactivada");
-    }
-    public void windowDeiconified(WindowEvent e){
-        System.out.println("Ventana Restaurada");
-    }
+//     public void windowActivated(WindowEvent e){
+//         System.out.println("Ventana Activada");
+//     }
+//     public void windowClosed(WindowEvent e){
+//         System.out.println("Ventana Cerrada");
+//     }
+//     public void windowClosing(WindowEvent e){
+//         System.out.println("Ventana Cerrandoce");
+//     }
+//     public void windowDeactivated(WindowEvent e){
+//         System.out.println("Ventana Desactivada");
+//     }
+//     public void windowDeiconified(WindowEvent e){
+//         System.out.println("Ventana Restaurada");
+//     }
+//     public void windowIconified(WindowEvent e){
+//         System.out.println("Ventana Minimizada");
+//     }
+//     public void windowOpened(WindowEvent e){
+//         System.out.println("Ventana Abierta");
+//     }
+
+// }
+
+// Ejemplo de clase oyente heredando de clase Adaptadora para no sobreescribir todos los métodos de una interfaz
+
+class M_Ventana extends WindowAdapter{
+	
+   
     public void windowIconified(WindowEvent e){
         System.out.println("Ventana Minimizada");
     }
-    public void windowOpened(WindowEvent e){
-        System.out.println("Ventana Abierta");
-    }
+   
 
 }
